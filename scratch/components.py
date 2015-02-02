a1619a915401bcef07c7b332f929864a8c61288b
Michele d'Amico <michele.damico@gmail.com>
2015-02-02 01:36:04 +0100

    Refactoring components by Block and BlockFactory

--- scratch/components.py
+++ scratch/components.py
@@ -6,6 +6,7 @@ from scratch.cgi import CGI
 
 __author__ = 'michele'
 
+
 def extract_arg(arg, kwargs, default=None):
     v = default
     if arg in kwargs:
@@ -14,21 +15,21 @@ def extract_arg(arg, kwargs, default=None):
     return v
 
 
-class SensorFactory():
-    type = "r"  # reporters
+class BlockFactory():
+    type = "u"  # unknown
+    block_constructor = None  # Abstract
+    cb_arg = None  # Abstract
 
-    def __init__(self, ed, name, default="", description=None):
+    def __init__(self, ed, name, description=None):
         """
         :param ed: The ExtensionDefinition (container)
-        :param name: the name of the sensor
-        :param default: the default return value
-        :param description: the description of the sensor. If None the description is equla to the name
+        :param name: the name of the block
+        :param description: the description of the block. If None the description is equal to the name
         :return:
         """
         self._ed = weakref.ref(ed) if ed is not None else None
         self._name = name
         self._description = description if description is not None else self._name
-        self._default = default
 
     @property
     def ed(self):
@@ -43,22 +44,59 @@ class SensorFactory():
         return self._description
 
     @property
-    def default(self):
-        return self._default
-
-    @property
     def definition(self):
         return [self.type, self.description, self.name]
 
     def create(self, extension, *args, **kwargs):
-        do_read = extract_arg("do_read", kwargs)
-        s = Sensor(extension, self, *args, **kwargs)
-        if do_read is not None:
-            s.do_read = do_read
-        return s
+        cb = extract_arg(self.cb_arg, kwargs) if self.cb_arg is not None else None
+        b = self.block_constructor(extension, self, *args, **kwargs)
+        if cb is not None:
+            setattr(b, self.cb_arg, cb)
+            b.do_read = cb
+        return b
 
 
-class Sensor():
+class Block():
+    """Abstract Extension components"""
+
+    def __init__(self, extension, info, value=None):
+        self._ex = weakref.ref(extension)
+        self._info = info
+        self._value = value if value is not None else info.default
+        self._lock = threading.Lock()
+
+    @property
+    def extension(self):
+        return self._ex()
+
+    @property
+    def info(self):
+        return self._info
+
+    @property
+    def type(self):
+        return self.info.type
+
+    @property
+    def name(self):
+        return self.info.name
+
+    @property
+    def description(self):
+        return self.info.description
+
+    @property
+    def definition(self):
+        return self.info.definition
+
+    def reset(self, request=None):
+        pass
+
+    def get_cgi(self, path):
+        return None
+
+
+class Sensor(Block):
     """Sensor sono gli elementi base delle estensioni: Vengono costruiti in base a una
     SensorFactory (del quale fanno da proxy per type, name, description es definition) es
     espongono i metodi get() es set() per leggere es scrivere il valore.
@@ -72,109 +110,39 @@ class Sensor():
         factory = SensorFactory(ed=None, name=name, default=default, description=description, **kwargs)
         return factory.create(extension=extension, do_read=do_read)
 
-    def __init__(self, extension, info, value=None):
-        self._ex = weakref.ref(extension)
-        self._info = info
-        self._value = value if value is not None else info.default
-        self.__lock = threading.Lock()
-
-    @property
-    def extension(self):
-        return self._ex()
-
-    @property
-    def info(self):
-        return self._info
-
-    @property
-    def type(self):
-        return self.info.type
-
-    @property
-    def name(self):
-        return self.info.name
-
-    @property
-    def description(self):
-        return self.info.description
-
-    @property
-    def definition(self):
-        return self.info.definition
-
     def get(self):
-        with self.__lock:
+        with self._lock:
             if hasattr(self, "do_read"):
                 self._value = self.do_read()
             return self._value
 
     def set(self, value):
-        with self.__lock:
+        with self._lock:
             self._value = value
 
-    def reset(self, request=None):
-        pass
 
-    def get_cgi(self, path):
-        return None
+class SensorFactory(BlockFactory):
+    type = "r"  # reporters
+    block_constructor = Sensor
+    cb_arg = "do_read"
 
-
-class CommandFactory():
-    type = " "  # no blocking commands
-
-    def __init__(self, ed, name, default=(), description=None, **kwargs):
+    def __init__(self, ed, name, default="", description=None):
         """
         :param ed: The ExtensionDefinition (container)
-        :param name: the name of command
-        :param defualt: the tuple of default values (used to describe component)
+        :param name: the name of the sensor
+        :param default: the default return value
         :param description: the description of the sensor. If None the description is equla to the name
-        :param kwargs: the menus entry lists
         :return:
         """
-        self._ed = weakref.ref(ed) if ed is not None else None
-        self._name = name
-        self._description = description if description is not None else self._name
+        super().__init__(ed=ed, name=name, description=description)
         self._default = default
-        self._menu_dict = kwargs
-        if not self._check_description():
-            raise ValueError("Wrong description and/or values/menus")
-
-    def _check_description(self):
-        return True
-
-    @property
-    def ed(self):
-        return self._ed() if self._ed else None
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def description(self):
-        return self._description
 
     @property
     def default(self):
         return self._default
 
-    @property
-    def menu_dict(self):
-        return self._menu_dict.copy()
 
-    @property
-    def definition(self):
-        return [self.type, self.description, self.name] + [d for d in self._default]
-
-    def create(self, extension, *args, **kwargs):
-        do_command = extract_arg("do_command", kwargs)
-        c = Command(extension, self, *args, **kwargs)
-        if do_command is not None:
-            c.do_command = do_command
-        return c
-
-
-class Command():
+class Command(Block):
     """Command components perform actions and return. User application should override
     the method do_command(*args) to do the real work.
     the property value return last value (or tuple if it haa more argument) and value_dict
@@ -185,41 +153,15 @@ class Command():
     def create(extension, name, default=(), description=None, **kwargs):
         do_command = extract_arg("do_command", kwargs)
         factory = CommandFactory(ed=None, name=name, default=default, description=description, **kwargs)
-        return factory.create(extension=extension,do_command=do_command)
+        return factory.create(extension=extension, do_command=do_command)
 
     def __init__(self, extension, info):
-        self._ex = weakref.ref(extension)
-        self._info = info
+        super().__init__(extension=extension, info=info)
         self._value = None
-        self.__lock = threading.Lock()
-
-    @property
-    def extension(self):
-        return self._ex()
-
-    @property
-    def info(self):
-        return self._info
-
-    @property
-    def type(self):
-        return self.info.type
-
-    @property
-    def name(self):
-        return self.info.name
-
-    @property
-    def description(self):
-        return self.info.description
-
-    @property
-    def definition(self):
-        return self.info.definition
 
     @property
     def value(self):
-        with self.__lock:
+        with self._lock:
             if self._value is None:
                 return None
             if len(self._value) == 1:
@@ -230,11 +172,8 @@ class Command():
         logging.info("command {} = {}".format(self.name, args))
         if hasattr(self, "do_command"):
             self.do_command(*args)
-        with self.__lock:
+        with self._lock:
             self._value = args
-
-    def reset(self, request=None):
-        pass
 
     @staticmethod
     def _get_request_data(path):
@@ -253,6 +192,42 @@ class Command():
         name, _args = self._get_request_data(path)
         if name == self.name:
             return CGI(self._cgi)
+
+
+class CommandFactory(BlockFactory):
+    type = " "  # no blocking commands
+    block_constructor = Command
+    cb_arg = "do_command"
+
+    def __init__(self, ed, name, default=(), description=None, **kwargs):
+        """
+        :param ed: The ExtensionDefinition (container)
+        :param name: the name of command
+        :param defualt: the tuple of default values (used to describe component)
+        :param description: the description of the sensor. If None the description is equla to the name
+        :param kwargs: the menus entry lists
+        :return:
+        """
+        super().__init__(ed=ed, name=name, description=description)
+        self._default = default
+        self._menu_dict = kwargs
+        if not self._check_description():
+            raise ValueError("Wrong description and/or values/menus")
+
+    def _check_description(self):
+        return True
+
+    @property
+    def default(self):
+        return self._default
+
+    @property
+    def menu_dict(self):
+        return self._menu_dict.copy()
+
+    @property
+    def definition(self):
+        return super().definition + [d for d in self._default]
 
 
 class Head():
